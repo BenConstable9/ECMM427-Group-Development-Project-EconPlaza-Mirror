5249f5d7ff4e1270de5bc37fbd6b7f25d09a79f6
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-10-23 14:34:16 +0100

    Write user model tests

---
+++ backend/backend/accounts/tests/models/test_user.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class UserCreationTest(TestCase):
+    def setUp(self):
+        """Initialise user model instance."""
+        User = get_user_model()
+        self.user = User.objects.create(username="user1")
+
+    def test_user_has_str(self):
+        """User has a defined __str__ method."""
+        self.assertEqual(self.user.__str__(), "user1")
