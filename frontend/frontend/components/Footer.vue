d65b6cf8983bfe032043ead7dd2630e2d423b51f
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-11-07 10:40:19 +0000

    Create example footer

---
+++ frontend/frontend/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <div id="navbar" class="bg-gray-100">
+      <div class="container mx-auto px-5">
+        <div id="links" class="flex space-x-8 py-3">
+          <ul class="flex">
+            <li class="text-sm">EconPlaza &copy; {{ date }}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      date: new Date().getFullYear(),
+    }
+  },
+}
+</script>
+
+<style></style>
