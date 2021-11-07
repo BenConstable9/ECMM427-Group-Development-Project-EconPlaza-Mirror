fbfa06d121d6153d606bc8f685396be9b8d0bdb1
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-11-07 10:40:14 +0000

    Create example header

---
+++ frontend/frontend/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <nav id="navbar" class="bg-gray-100">
+      <div class="container mx-auto px-5">
+        <div id="links" class="flex space-x-8 py-3">
+          <ul class="flex">
+            <li class="font-semibold">EconPlaza</li>
+          </ul>
+          <ul class="flex flex-1 space-x-8">
+            <li>Home</li>
+            <li>General</li>
+            <li>My plazas &#x25BC;</li>
+          </ul>
+          <ul class="flex space-x-8">
+            <li>Log in</li>
+            <li>Sign up</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
