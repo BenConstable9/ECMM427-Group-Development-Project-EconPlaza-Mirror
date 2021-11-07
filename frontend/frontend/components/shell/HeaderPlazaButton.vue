7f73a8ddc69959e3e8d6354537e5b3e3b3f7a83f
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-11-07 14:48:09 +0000

    Convert plaza buttons to hyperlinks

--- frontend/frontend/components/shell/HeaderPlazaButton.vue
+++ frontend/frontend/components/shell/HeaderPlazaButton.vue
@@ -1,18 +1,20 @@
 <template>
-  <li
-    class="
-      text-sm text-gray-900
-      bg-gray-50
-      hover:bg-gray-100
-      cursor-pointer
-      border
-      rounded-full
-      overflow-hidden
-      px-3
-      py-0.5
-    "
-  >
-    {{ plazaName }}
+  <li>
+    <a
+      class="
+        text-sm text-gray-900
+        bg-gray-50
+        hover:bg-gray-100
+        border
+        rounded-full
+        overflow-hidden
+        px-3
+        py-0.5
+      "
+      href="#"
+    >
+      {{ plazaName }}
+    </a>
   </li>
 </template>
 
