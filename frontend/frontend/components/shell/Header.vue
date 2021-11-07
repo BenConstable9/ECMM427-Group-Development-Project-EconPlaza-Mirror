73fc51174d73e69dadffd7f64614bd55829a69d9
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-11-07 13:42:01 +0000

    Split plaza link buttons from navbar

--- frontend/frontend/components/shell/Header.vue
+++ frontend/frontend/components/shell/Header.vue
@@ -2,18 +2,27 @@
   <header>
     <nav id="navbar" class="bg-gray-100">
       <div class="container mx-auto px-5">
-        <div id="links" class="flex space-x-8 py-3">
-          <ul class="flex">
-            <li class="font-semibold">EconPlaza</li>
-          </ul>
-          <ul class="flex flex-1 space-x-8">
-            <li>Home</li>
-            <li>General</li>
-            <li>My plazas &#x25BC;</li>
+        <div id="links" class="flex items-center space-x-8 py-3">
+          <ul class="flex flex-1">
+            <li class="text-xl text-gray-900">EconPlaza</li>
           </ul>
           <ul class="flex space-x-8">
-            <li>Log in</li>
-            <li>Sign up</li>
+            <li>
+              <a class="text-gray-900 hover:underline" href="#">Log in</a>
+            </li>
+            <li>
+              <a class="text-gray-900 hover:underline" href="#">Sign up</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <nav id="plazas" class="bg-blue-50">
+      <div class="container mx-auto px-5">
+        <div id="links" class="flex space-x-8 py-2">
+          <ul class="flex flex-1 space-x-2">
+            <header-plaza-button plazaName="Home" />
+            <header-plaza-button plazaName="General" />
           </ul>
         </div>
       </div>
@@ -22,7 +31,10 @@
 </template>
 
 <script>
-export default {}
+import HeaderPlazaButton from './HeaderPlazaButton.vue'
+export default {
+  components: { HeaderPlazaButton },
+}
 </script>
 
 <style></style>
---
+++ frontend/frontend/components/shell/HeaderPlazaButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <li
+    class="
+      text-sm text-gray-900
+      bg-gray-50
+      hover:bg-gray-100
+      cursor-pointer
+      border
+      rounded-full
+      overflow-hidden
+      px-3
+      py-0.5
+    "
+  >
+    {{ plazaName }}
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    plazaName: { type: String, default: 'Plaza' },
+  },
+}
+</script>
+
+<style></style>
