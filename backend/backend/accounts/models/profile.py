ab119ef048f380f41da643c9c4f40809d8ca0975
Ben Constable <ben-constable@outlook.com>
2021-11-27 16:29:23 +0000

    Add user and profile DB design

--- backend/backend/accounts/models/profile.py
+++ backend/backend/accounts/models/profile.py
@@ -21,9 +21,16 @@ class Profile(models.Model):
         "Profile's reputation (Computed)", default=0
     )
 
+    display_name = models.CharField("Profile's Display Name", max_length=30)
+
+    global_anonymous = models.BooleanField("Profile's Annoymous Flag", default=0)
+
+    # Assumes reputation can never be negative
+    reputation = models.PositiveBigIntegerField("Profile's reputation (computed)", default=0)
+
     created_at = models.DateTimeField("Created at timestamp", auto_now_add=True)
 
-    last_computed = models.DateTimeField("Last modified timestamp", auto_now_add=True)
+    last_computed = models.DateTimeField("Last modified timestamp")
 
     def __str__(self):
         return f"{self.user}"
