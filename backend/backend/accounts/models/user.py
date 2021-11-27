9caf2ca0b14bf83c72ffa3e7552783032dc2cd6e
Ben Constable <ben-constable@outlook.com>
2021-11-27 16:29:23 +0000

    Add user and profile DB design

--- backend/backend/accounts/models/user.py
+++ backend/backend/accounts/models/user.py
@@ -3,5 +3,12 @@ from django.db import models
 
 
 class User(AbstractUser):
+
+    verified = models.BooleanField("User's Verified Flag", default=0)
+
+    banned = models.BooleanField("User's Banned Flag", default=0)
+
+    twitter_oauth = models.CharField("User's Twitter OAuth", max_length=16)
+
     def __str__(self):
         return f"{self.username}"
