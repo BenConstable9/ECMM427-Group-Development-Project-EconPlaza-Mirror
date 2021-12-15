3ad8aa81358ade7a6b42bf91cceefc1e133d395b
Ben Constable <ben-constable@outlook.com>
2021-12-15 12:07:17 +0000

    Add the custom user model to the admin view

--- backend/backend/accounts/admin/user.py
+++ backend/backend/accounts/admin/user.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth import get_user_model
+from ..models import User
 
-User = get_user_model()
-
-# Taken from: https://stackoverflow.com/questions/48011275/custom-user-model-fields-abstractuser-not-showing-in-django-admin
 fields = list(UserAdmin.fieldsets)
-fields[0] = (None, {"fields": ("banned", "verified")})
+fields[0] = (None, {'fields': ('banned', 'verified')})
 UserAdmin.fieldsets = tuple(fields)
 
 admin.site.register(User, UserAdmin)
