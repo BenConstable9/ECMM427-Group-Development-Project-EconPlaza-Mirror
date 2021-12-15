3a25794fe7eeb8881fb73588c0826c502ba722ff
Ben Constable <ben-constable@outlook.com>
2021-12-15 16:24:44 +0000

    Add serializer test

--- backend/backend/accounts/tests/serializers/test_vouch.py
+++ backend/backend/accounts/tests/serializers/test_vouch.py
@@ -1,16 +1,12 @@
-from django.contrib.auth import get_user_model
-from rest_framework.test import APIRequestFactory, APITestCase
-from rest_framework.request import Request
-from ...models import Vouch
+from rest_framework.test import APITestCase
+
+from ...models import Vouch, User
 
 from ...serializers import VouchSerializer
-
 
 class VouchSerializerTest(APITestCase):
     def setUp(self):
         """Initalise a series of users and a vouching."""
-
-        User = get_user_model()
 
         self.user_1 = User.objects.create(username="user_1")
 
@@ -18,29 +14,20 @@ class VouchSerializerTest(APITestCase):
 
         self.vouch = Vouch.objects.create(voucher=self.user_1, vouchee=self.user_2)
 
-        factory = APIRequestFactory()
-        request = factory.get("/v1/accounts/vouches/{}/".format(self.user_2.id))
-
-        serializer_context = {
-            "request": Request(request),
-        }
-
         # Serialise the data
-        self.serializer = VouchSerializer(
-            instance=self.vouch, context=serializer_context
-        )
+        self.serializer = VouchSerializer(instance=self.vouch)
 
     def test_contains_expected_fields(self):
         """Test the serialiser returns the expected fields."""
 
         data = self.serializer.data
 
-        self.assertEqual(set(data.keys()), set(["vouchee", "voucher"]))
+        self.assertEqual(set(data.keys()), set(['vouchee', 'voucher']))
 
     def test_equal_data(self):
         """Test the serialiser returns the expected values."""
 
         data = self.serializer.data
 
-        self.assertEqual(data["vouchee"], self.user_2.id)
-        self.assertEqual(data["voucher"]["id"], self.user_1.id)
+        self.assertEqual(data['vouchee'], self.user_2.id)
+        self.assertEqual(data['voucher'], self.user_1.id)
