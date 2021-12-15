938f91d80b28dbabbbe60b7940b8e2394392a179
Ben Constable <ben-constable@outlook.com>
2021-12-15 16:24:44 +0000

    Add serializer test

---
+++ backend/backend/accounts/tests/serializers/test_vouch.py
@@ -0,0 +1,33 @@
+from rest_framework.test import APITestCase
+
+from ...models import Vouch, User
+
+from ...serializers import VouchSerializer
+
+class VouchSerializerTest(APITestCase):
+    def setUp(self):
+        """Initalise a series of users and a vouching."""
+
+        self.user_1 = User.objects.create(username="user_1")
+
+        self.user_2 = User.objects.create(username="user_2")
+
+        self.vouch = Vouch.objects.create(voucher=self.user_1, vouchee=self.user_2)
+
+        # Serialise the data
+        self.serializer = VouchSerializer(instance=self.vouch)
+
+    def test_contains_expected_fields(self):
+        """Test the serialiser returns the expected fields."""
+
+        data = self.serializer.data
+
+        self.assertEqual(set(data.keys()), set(['vouchee', 'voucher']))
+
+    def test_equal_data(self):
+        """Test the serialiser returns the expected values."""
+
+        data = self.serializer.data
+
+        self.assertEqual(data['vouchee'], self.user_2.id)
+        self.assertEqual(data['voucher'], self.user_1.id)
