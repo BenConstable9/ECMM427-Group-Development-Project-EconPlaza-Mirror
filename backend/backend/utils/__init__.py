efb89bbfe4d28a8ffe44219e119161217a5ad7b5
Ben Constable <ben-constable@outlook.com>
2021-12-16 19:51:55 +0000

    Add permissions based on the action

--- backend/backend/utils/__init__.py
+++ backend/backend/utils/__init__.py
@@ -1,4 +1,1 @@
 from .permissions import ActionBasedPermission
-from .permissions import IsVerified
-from .permissions import IsSelf
-from .pagination import StandardResultsSetPagination
--- backend/backend/utils/permissions.py
+++ backend/backend/utils/permissions.py
@@ -1,41 +1,13 @@
-from rest_framework.permissions import SAFE_METHODS, AllowAny
-from rest_framework.permissions import AllowAny, IsAuthenticated
-
+from rest_framework.permissions import AllowAny
 
 # Taken form https://stackoverflow.com/questions/39392007/django-rest-framework-viewset-permissions-create-without-list
-
 
 class ActionBasedPermission(AllowAny):
     """
     Grant or deny access to a view, based on a mapping in view.action_permissions
     """
-
     def has_permission(self, request, view):
-        for klass, actions in getattr(view, "action_permissions", {}).items():
+        for klass, actions in getattr(view, 'action_permissions', {}).items():
             if view.action in actions:
                 return klass().has_permission(request, view)
         return False
-
-
-class IsVerified(AllowAny):
-    """
-    Custom permission to only allow verified users to vouch.
-    """
-
-    def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            # Let through GET, HEAD and OPTIONS
-            if request.method in SAFE_METHODS:
-                return True
-            else:
-                # Check if verified
-                return request.user.verified
-        else:
-            return False
-
-
-class IsSelf(AllowAny):
-    """Custom permission to only allow requests to /users/1/slug when the user is theirself"""
-
-    def has_permission(self, request, view):
-        return request.user.id == int(view.kwargs["users_pk"])
