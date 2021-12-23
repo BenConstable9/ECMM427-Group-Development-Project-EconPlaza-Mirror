68ef7258b35c1070329ba64ac95de2960fbaeb53
Ben Constable <ben-constable@outlook.com>
2021-12-23 15:15:57 +0000

    Use get_user_model

--- backend/backend/accounts/serializers/user.py
+++ backend/backend/accounts/serializers/user.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 
-from ..models import User
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = User
-        fields = ["id", "url", "username", "email", "verified", "is_staff", "first_name", "last_name", "date_joined"]
+        model = get_user_model()
+        fields = ["id", "url", "username", "email", "verified",
+                  "is_staff", "first_name", "last_name", "date_joined"]
