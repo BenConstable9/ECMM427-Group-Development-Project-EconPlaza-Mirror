bfd53745fcf5df42fc4ce9a3b3475c0c9ba13792
Ben Constable <ben-constable@outlook.com>
2021-12-30 12:38:16 +0000

    Updated footer to show total vouches

--- frontend/frontend/components/vouch/vouch-box-footer.vue
+++ frontend/frontend/components/vouch/vouch-box-footer.vue
@@ -1,16 +1,13 @@
 <template>
-  <div class="bg-gray-200 px-4 py-2">Do you</div>
+  <div class="bg-gray-200 px-4 py-2">
+    Total Vouches: <span>{{ vouches.length }}</span>
+  </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-
 export default {
   name: 'VouchBoxDetails',
   props: { vouches: { type: Array, default: null } },
-  computed: {
-    ...mapGetters(['authenticatedUser']),
-  },
 }
 </script>
 
