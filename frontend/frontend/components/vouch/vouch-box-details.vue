6b5ea36c5e1409a994bf08887a81007ebb722b9c
Ben Constable <ben-constable@outlook.com>
2021-12-30 12:33:11 +0000

    Add a basic user profile page

--- frontend/frontend/components/vouch/vouch-box-details.vue
+++ frontend/frontend/components/vouch/vouch-box-details.vue
@@ -1,35 +1,34 @@
 <template>
-    <div class="bg-gray-100 px-4 py-2">
-        <ul v-if="vouches.length > 0" class="list-none">
-            <div
-                v-for="vouch in vouches"
-                :key="vouch.voucher.id"
-                class="flex items-start border-b py-2"
-            >
-                <div class="flex-1 md:flex-none lg:flex-1">
-                    <NuxtLink :to="`/users/${vouch.voucher.id}`">
-                        <h4 class="text-md font-semibold text-blue-400">
-                            {{ vouch.voucher.first_name }}
-                            {{ vouch.voucher.last_name }}
-                        </h4>
-                    </NuxtLink>
-                </div>
-                <verified
-                    v-if="vouch.voucher.verified"
-                    class="flex-initial md:flex-none lg:flex-initial"
-                />
-                <staff
-                    v-if="vouch.voucher.isStaff"
-                    class="flex-initial md:flex-none lg:flex-initial"
-                />
-            </div>
-        </ul>
-        <div v-else class="py-2">
-            <h4 class="text-md font-semibold text-red-400">
-                No one has vouched yet!
+  <div class="bg-gray-100 px-4 py-2">
+    <ul v-if="vouches.length > 0" class="list-none">
+      <div
+        v-for="vouch in vouches"
+        :key="vouch.voucher.id"
+        class="flex items-start border-b py-2"
+      >
+        <div class="flex-1 md:flex-none lg:flex-1">
+          <NuxtLink :to="`/users/${vouch.voucher.id}`">
+            <h4 class="text-md font-semibold text-blue-400">
+              {{ vouch.voucher.first_name }} {{ vouch.voucher.last_name }}
             </h4>
+          </NuxtLink>
         </div>
+        <verified
+          v-if="vouch.voucher.verified"
+          class="flex-initial md:flex-none lg:flex-initial"
+        />
+        <staff
+          v-if="vouch.voucher.isStaff"
+          class="flex-initial md:flex-none lg:flex-initial"
+        />
+      </div>
+    </ul>
+    <div v-else class="py-2">
+      <h4 class="text-md font-semibold text-red-400">
+        No one has vouched yet!
+      </h4>
     </div>
+  </div>
 </template>
 
 <script>
@@ -37,12 +36,12 @@ import Verified from '~/components/labels/verified'
 import Staff from '~/components/labels/staff'
 
 export default {
-    name: 'VouchBoxDetails',
-    components: {
-        Verified,
-        Staff,
-    },
-    props: { vouches: { type: Array, default: null } },
+  name: 'VouchBoxDetails',
+  components: {
+    Verified,
+    Staff,
+  },
+  props: { vouches: { type: Array, default: null } },
 }
 </script>
 
--- frontend/frontend/components/vouch/vouch-box.vue
+++ frontend/frontend/components/vouch/vouch-box.vue
@@ -1,37 +1,45 @@
 <template>
-    <div>
-        <ul
-            class="
-                w-full
-                border
-                rounded-r-2xl rounded-b-2xl
-                overflow-hidden
-                shadow-md
-            "
-        >
-            <vouch-box-header :first-name="firstName" />
-            <vouch-box-details :vouches="vouches" />
-            <vouch-box-footer :vouches="vouches" />
-        </ul>
-    </div>
+  <div>
+    <ul
+      class="
+        w-full
+        border
+        rounded-r-2xl rounded-b-2xl
+        overflow-hidden
+        shadow-md
+      "
+    >
+      <vouch-box-header :first-name="firstName" />
+      <vouch-box-details :vouches="vouches" />
+      <vouch-box-verify
+        v-if="authenticatedUser.is_staff || authenticatedUser.verified"
+        :vouches="vouches"
+      />
+    </ul>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 import VouchBoxDetails from '~/components/vouch/vouch-box-details'
 import VouchBoxHeader from '~/components/vouch/vouch-box-header'
-import VouchBoxFooter from '~/components/vouch/vouch-box-footer'
+import VouchBoxVerify from '~/components/vouch/vouch-box-verify'
 
 export default {
-    name: 'VouchBox',
-    components: {
-        VouchBoxDetails,
-        VouchBoxHeader,
-        VouchBoxFooter,
-    },
-    props: {
-        firstName: { type: String, default: null },
-        vouches: { type: Array, default: null },
-    },
+  name: 'VouchBox',
+  components: {
+    VouchBoxDetails,
+    VouchBoxHeader,
+    VouchBoxVerify,
+  },
+  props: {
+    firstName: { type: String, default: null },
+    vouches: { type: Array, default: null },
+  },
+  computed: {
+    ...mapGetters(['authenticatedUser']),
+  },
 }
 </script>
 
