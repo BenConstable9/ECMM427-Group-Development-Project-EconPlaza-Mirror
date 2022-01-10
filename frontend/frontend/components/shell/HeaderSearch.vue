555b53dede06beed098506bfa7ae2c3fdaf2f5f9
LloydTao <LL23@live.co.uk>
2022-01-10 17:55:25 +0000

    Reorder svg attributes

--- frontend/frontend/components/shell/HeaderSearch.vue
+++ frontend/frontend/components/shell/HeaderSearch.vue
@@ -20,11 +20,11 @@
     />
     <button type="submit" class="absolute right-0 top-0 mt-2 mr-4">
       <svg
+        id="Capa_1"
         class="text-gray-600 h-4 w-4 fill-current"
         xmlns="http://www.w3.org/2000/svg"
         xmlns:xlink="http://www.w3.org/1999/xlink"
         version="1.1"
-        id="Capa_1"
         x="0px"
         y="0px"
         viewBox="0 0 56.966 56.966"
