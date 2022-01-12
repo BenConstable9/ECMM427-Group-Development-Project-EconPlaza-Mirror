71aed21889f44849dc2bdbd1770bb519939a3cf3
Ben Constable <ben-constable@outlook.com>
2022-01-12 18:47:14 +0000

    Add fiorm handling for vouching

--- frontend/frontend/components/vouch/voucher.vue
+++ frontend/frontend/components/vouch/voucher.vue
@@ -12,31 +12,44 @@
       <voucher-header :first-name="firstName" />
       <Error v-if="error" :message="error" />
       <div class="bg-gray-100 px-4 py-2">
-        <form class="space-y-4" @submit.prevent="userLogin">
+        <form class="space-y-4" @submit.prevent="userVouch">
           <div>
-            <strong
+            <label
               >Are you certain that this name belongs to a real person who you
-              know?</strong
+              know?</label
             >
-            <input v-model="voucher.name" type="checkbox" />
+            <input
+              v-model="voucher.name"
+              type="checkbox"
+              :disabled="voucher.isDisabled"
+            />
           </div>
           <div>
-            <strong
+            <label
               >Are you 100% certain that this email belongs to the person named
-              above?</strong
+              above?</label
             >
-            <input v-model="voucher.email" type="checkbox" />
+            <input
+              v-model="voucher.email"
+              type="checkbox"
+              :disabled="voucher.isDisabled"
+            />
           </div>
           <div>
-            <strong
+            <label
               >Are you 100% certain the institutional affiliation and position
-              of the person named above are correct?</strong
+              of the person named above are correct?</label
             >
-            <input v-model="voucher.institution" type="checkbox" />
+            <input
+              v-model="voucher.institution"
+              type="checkbox"
+              :disabled="voucher.isDisabled"
+            />
           </div>
           <div v-if="voucher.name && voucher.email && voucher.institution">
             <button
               type="submit"
+              :disabled="voucher.isDisabled"
               class="
                 w-full
                 py-4
@@ -73,6 +86,7 @@ export default {
     VoucherHeader,
   },
   props: {
+    id: { type: Number, default: null },
     firstName: { type: String, default: null },
     vouches: { type: Array, default: null },
   },
@@ -82,10 +96,34 @@ export default {
         name: false,
         email: false,
         institution: false,
-        isDisabled: true,
+        isDisabled: false,
       },
       error: null,
     }
+  },
+  methods: {
+    async userVouch() {
+      this.voucher.isDisabled = true
+
+      // Send to server
+      await this.$axios
+        .post('/v1/accounts/vouches/', {
+          vouchee: this.id,
+          voucher: this.$store.getters.authenticatedUser.id,
+        })
+        .then(this.$nuxt.$emit('user-vouched'))
+        .catch((response) => {
+          if (typeof response === 'string' || response instanceof String) {
+            this.error = response
+          } else if ('detail' in response) {
+            this.error = response.detail
+          } else {
+            this.error = 'Unable to process request.'
+          }
+
+          this.voucher.isDisabled = false
+        })
+    },
   },
 }
 </script>
