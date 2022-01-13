148e34086ce26d374b3cdf44b95161420edaa63e
LloydTao <LL23@live.co.uk>
2022-01-13 12:42:34 +0000

    Use separate component for home feed

--- frontend/frontend/components/home/FeedTable.vue
+++ frontend/frontend/components/home/FeedTable.vue
@@ -1,14 +1,14 @@
 <template>
-    <div id="feed" class="flex">
-        <ul class="flex flex-col w-full border rounded-lg overflow-hidden">
-            <feed-table-header />
-            <feed-table-row
-                v-for="i in 8"
-                :key="i"
-                :class="{ 'bg-gray-50': i % 2 }"
-            />
-        </ul>
-    </div>
+  <div id="posts" class="flex">
+    <ul class="flex flex-col w-full border rounded-lg overflow-hidden">
+      <post-table-header />
+      <post-table-row
+        v-for="i in 8"
+        :key="i"
+        :class="{ 'bg-gray-50': i % 2 }"
+      />
+    </ul>
+  </div>
 </template>
 
 <script></script>
