177048eb9aa07f0afa0fbea9bd2b0b876817f08d
LloydTao <LL23@live.co.uk>
2022-01-13 12:40:51 +0000

    Use new sidebar for home

---
+++ frontend/frontend/components/home/MyPlazasRow.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="rules-info" class="flex items-center px-3 py-2">
+    <li class="text-sm text-gray-900">Plaza</li>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
