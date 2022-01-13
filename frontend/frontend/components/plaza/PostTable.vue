e68a82ae741de53b436a6a2e15b9fe7d01a097e4
LloydTao <LL23@live.co.uk>
2022-01-13 11:48:10 +0000

    Make post table like prototype

--- frontend/frontend/components/plaza/PostTable.vue
+++ frontend/frontend/components/plaza/PostTable.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="posts" class="flex">
-    <ul class="flex flex-col w-full border rounded overflow-hidden">
+    <ul class="flex flex-col w-full border rounded-lg overflow-hidden">
       <post-table-header />
       <post-table-row
         v-for="i in 8"
         :key="i"
-        :class="{ 'bg-gray-100': i % 2 }"
+        :class="{ 'bg-gray-50': i % 2 }"
       />
     </ul>
   </div>
