e8b3b8d53cdf0d231048bd291f712722b9d7222e
LloydTao <LL23@live.co.uk>
2022-01-13 11:59:35 +0000

    Refresh plaza about box

---
+++ frontend/frontend/components/plaza/AboutBoxStats.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="about-info" class="flex bg-gray-50 px-5 py-3">
+    <div class="flex-1 text-gray-900">
+      <p class="text-sm">Members</p>
+      <p>23</p>
+    </div>
+    <div class="flex-1 text-gray-900">
+      <p class="text-sm">Posts</p>
+      <p>814</p>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
