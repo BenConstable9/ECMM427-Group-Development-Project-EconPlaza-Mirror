f9b06a0480e6046d55aa6ee54815180ad5702826
Ben Constable <ben-constable@outlook.com>
2022-01-13 14:13:59 +0000

    Move the permission to a common place
    Remove the isAdmin as all admins automatically become verified

--- backend/backend/utils/__init__.py
+++ backend/backend/utils/__init__.py
@@ -1,2 +1,4 @@
+from .permissions import ActionBasedPermission
+from .permissions import IsVerified
 from .pagination import StandardResultsSetPagination
 from .permissions import ActionBasedPermission
