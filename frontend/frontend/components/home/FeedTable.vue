c7ba053464001b6a8689489ad1e2130265345ab8
LloydTao <LL23@live.co.uk>
2022-01-13 12:44:18 +0000

    Fix post table and feed table children

--- frontend/frontend/components/home/FeedTable.vue
+++ frontend/frontend/components/home/FeedTable.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="posts" class="flex">
+  <div id="feed" class="flex">
     <ul class="flex flex-col w-full border rounded-lg overflow-hidden">
-      <post-table-header />
-      <post-table-row
+      <feed-table-header />
+      <feed-table-row
         v-for="i in 8"
         :key="i"
         :class="{ 'bg-gray-50': i % 2 }"
