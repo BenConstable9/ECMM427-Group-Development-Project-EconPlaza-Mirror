05da8fab3d5e62293cfb76534ebba94a0dbffb43
LloydTao <LL23@live.co.uk>
2022-01-13 11:59:35 +0000

    Refresh plaza about box

--- frontend/frontend/components/plaza/AboutBox.vue
+++ frontend/frontend/components/plaza/AboutBox.vue
@@ -1,10 +1,10 @@
 <template>
-    <div id="about" class="flex">
-        <ul class="flex flex-col w-full border rounded-lg overflow-hidden">
-            <about-box-header />
-            <about-box-stats />
-        </ul>
-    </div>
+  <div id="about" class="flex">
+    <ul class="flex flex-col w-full border rounded-lg overflow-hidden">
+      <about-box-header />
+      <about-box-stats />
+    </ul>
+  </div>
 </template>
 
 <script></script>
--- frontend/frontend/components/plaza/AboutBoxStats.vue
+++ frontend/frontend/components/plaza/AboutBoxStats.vue
@@ -1,45 +1,16 @@
 <template>
-    <div id="about-info" class="flex bg-gray-50 px-5 py-3">
-        <div class="flex-1 text-gray-900">
-            <p class="text-sm">Members</p>
-            <p>{{ members }}</p>
-        </div>
-        <div class="flex-1 text-gray-900">
-            <p class="text-sm">Posts</p>
-            <p>{{ posts }}</p>
-        </div>
+  <div id="about-info" class="flex bg-gray-50 px-5 py-3">
+    <div class="flex-1 text-gray-900">
+      <p class="text-sm">Members</p>
+      <p>23</p>
     </div>
+    <div class="flex-1 text-gray-900">
+      <p class="text-sm">Posts</p>
+      <p>814</p>
+    </div>
+  </div>
 </template>
 
-<script>
-import { mapGetters, mapActions } from 'vuex'
-
-export default {
-    data() {
-        return {
-            loading: false,
-        }
-    },
-    computed: {
-        ...mapGetters({ plaza: 'plazas/current' }),
-        members() {
-            return this.plaza ? this.plaza.stats.members : ''
-        },
-        posts() {
-            return this.plaza ? this.plaza.stats.posts : ''
-        },
-    },
-    async created() {
-        this.loading = true
-        await this.getCurrentPlaza(this.$route.params.plazas)
-        this.loading = false
-    },
-    methods: {
-        ...mapActions({
-            getCurrentPlaza: 'plazas/getCurrentPlaza',
-        }),
-    },
-}
-</script>
+<script></script>
 
 <style></style>
