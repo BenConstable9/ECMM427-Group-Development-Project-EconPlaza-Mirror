88aa2e0fc2e9a9fa903b72d27194f5496b681049
LloydTao <LL23@live.co.uk>
2022-01-13 12:09:09 +0000

    Tweak footer styles

--- frontend/frontend/components/shell/Footer.vue
+++ frontend/frontend/components/shell/Footer.vue
@@ -1,15 +1,11 @@
 <template>
-    <footer>
-        <div id="footer" class="bg-gray-50">
-            <div class="container mx-auto px-5">
-                <div id="links" class="flex space-x-8 py-5">
-                    <ul class="flex">
-                        <li class="text-sm text-gray-500">
-                            EconPlaza &copy; {{ date }}
-                        </li>
-                    </ul>
-                </div>
-            </div>
+  <footer>
+    <div id="navbar" class="bg-gray-50">
+      <div class="container mx-auto px-5">
+        <div id="links" class="flex space-x-8 py-5">
+          <ul class="flex">
+            <li class="text-sm text-gray-500">EconPlaza &copy; {{ date }}</li>
+          </ul>
         </div>
     </footer>
 </template>
