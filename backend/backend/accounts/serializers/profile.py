7ac753d31cfc8531a16bddf47a89d19212373a5f
Ben Constable <ben-constable@outlook.com>
2022-01-17 15:55:51 +0000

    Add profile API url

--- backend/backend/accounts/serializers/profile.py
+++ backend/backend/accounts/serializers/profile.py
@@ -2,13 +2,9 @@ from rest_framework import serializers
 from ..models import Profile
 
 
-class ProfileSerializer(serializers.ModelSerializer):
+class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Profile
-        fields = [
-            "id",
-            "display_name",
-            "global_anonymous",
-            "reputation",
-            "created_at",
-        ]
+        fields = ["display_name",
+                  "global_anonymous", "reputation", "created_at"]
--- backend/backend/accounts/viewsets/profile.py
+++ backend/backend/accounts/viewsets/profile.py
@@ -1,26 +1,16 @@
 from rest_framework import viewsets, mixins
 from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth import get_user_model
 
 from ..serializers import ProfileSerializer
 from ..models import Profile
 
-from utils import IsSelf
 
-
-class ProfileViewSet(
-    mixins.RetrieveModelMixin,
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet,
-):
+class ProfileViewSet(mixins.RetrieveModelMixin,
+                     viewsets.GenericViewSet,):
     """
     API endpoint that allows the viewing of a specific plaza
     """
 
-    def get_queryset(self):
-        # If we aren't looking at ones self
-        user = get_user_model().objects.get(id=self.kwargs["users_pk"])
-        return Profile.objects.filter(user=user)
-
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    permission_classes = [IsAuthenticated, IsSelf]
+    permission_classes = [IsAuthenticated]
