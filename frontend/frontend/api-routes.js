e9a953e1c1e93139c12f1542793900e7d68ed2cf
Ben Constable <ben-constable@outlook.com>
2022-01-17 20:28:41 +0000

    Add profile store

--- frontend/frontend/api-routes.js
+++ frontend/frontend/api-routes.js
@@ -6,3 +6,10 @@ export const PLAZAS = {
   ALL: () => '/v1/plazas/',
   ONE: (slug) => `/v1/plazas/${slug}/`,
 }
+
+// Profiles
+
+export const PROFILES = {
+  ALL: () => '/v1/accounts/profiles/',
+  ONE: (id) => `/v1/accounts/profiles/${id}/`,
+}
