0c5f8cccee0495301a43fe882723c86bda28f27f
Ben Constable <ben-constable@outlook.com>
2022-01-17 16:57:14 +0000

    Format

--- backend/backend/plazas/viewsets/post.py
+++ backend/backend/plazas/viewsets/post.py
@@ -8,28 +8,29 @@ from ..serializers import PostSerializer
 from ..models import Plaza, Post
 
 
-class PostViewSet(mixins.CreateModelMixin,
-                  mixins.ListModelMixin,
-                  mixins.RetrieveModelMixin,
-                  viewsets.GenericViewSet,):
+class PostViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
     """
     API endpoint that allows users to view Posts and create new ones
     """
 
     def get_queryset(self):
-        plaza = Plaza.objects.get(slug=self.kwargs['plazas_slug'])
+        plaza = Plaza.objects.get(slug=self.kwargs["plazas_slug"])
         return Post.objects.filter(plaza=plaza.id)
 
     serializer_class = PostSerializer
 
     permission_classes = (ActionBasedPermission,)
-    action_permissions = {IsVerified: [
-        "create"], IsAuthenticated: ["retrieve", "list"]}
+    action_permissions = {IsVerified: ["create"], IsAuthenticated: ["retrieve", "list"]}
 
     lookup_field = "id"
 
     def perform_create(self, serializer):
-        plaza = Plaza.objects.get(slug=self.kwargs['plazas_slug'])
+        plaza = Plaza.objects.get(slug=self.kwargs["plazas_slug"])
 
         # Force the user to be the logged in user and the plaza to be from the slug
         serializer.save(user=self.request.user, plaza=plaza)
