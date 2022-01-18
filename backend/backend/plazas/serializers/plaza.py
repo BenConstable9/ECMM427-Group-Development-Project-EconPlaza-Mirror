7111409787cd1cb241540836c18f639cc23fa398
Ben Constable <ben-constable@outlook.com>
2022-01-18 19:41:52 +0000

    Lint

--- backend/backend/plazas/serializers/plaza.py
+++ backend/backend/plazas/serializers/plaza.py
@@ -6,17 +6,25 @@ import json
 
 class PlazaSerializer(serializers.HyperlinkedModelSerializer):
     permissions = serializers.JSONField()
-    stats = serializers.SerializerMethodField('get_plaza_stats')
+    stats = serializers.SerializerMethodField("get_plaza_stats")
 
     class Meta:
         model = Plaza
-        fields = ["id", "slug", "name", "description", "created_at", "permissions", "stats"]
+        fields = [
+            "id",
+            "slug",
+            "name",
+            "description",
+            "created_at",
+            "permissions",
+            "stats",
+        ]
         lookup_field = "slug"
 
     def get_plaza_stats(self, instance):
         return {
-            'members': Member.objects.all().filter(plaza=instance).count(),
-            'posts': Post.objects.all().filter(plaza=instance).count(),
+            "members": Member.objects.all().filter(plaza=instance).count(),
+            "posts": Post.objects.all().filter(plaza=instance).count(),
         }
 
     def to_representation(self, instance):
