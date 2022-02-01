ff0b9dc4c7bb1f4d9ead093503dac20d31c285bd
Owen Saunders <owensaundersemail@gmail.com>
2022-02-01 14:25:48 +0000

    Add error component

---
+++ frontend/frontend/components/helpers/error.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="
+      w-full
+      p-4
+      bg-red-700
+      rounded-r-2xl rounded-b-2xl
+      text-m
+      font-bold
+      text-white
+      shadow
+    "
+  >
+    Error: {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error',
+  props: {
+    message: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
