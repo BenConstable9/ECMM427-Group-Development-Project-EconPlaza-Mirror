a0bc4532de07b48d3328cbdd20dba8a0f6ac9577
Owen Saunders <owensaundersemail@gmail.com>
2022-02-01 16:15:50 +0000

    Add creation serializer

--- backend/backend/accounts/viewsets/user.py
+++ backend/backend/accounts/viewsets/user.py
@@ -1,22 +1,31 @@
 from rest_framework import viewsets, mixins, permissions, filters
+from rest_framework.permissions import SAFE_METHODS
 from django.contrib.auth import get_user_model
 from utils import StandardResultsSetPagination
 
 from ..serializers import UserSerializer
+from ..serializers.user import UserPostSerializer
+from ..permissions.create_or_authenticate import IsCreationOrIsAuthenticated
 
 # Only allow GET, HEAD and OPTIONS requests
 
 
 class UserViewSet(
-    mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+   mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
 ):
     """
-    API endpoint that allows for viewing of users.
+    API endpoint that allows for viewing and creating of users.
     """
 
     queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.request.method in SAFE_METHODS:
+            return UserSerializer
+        else:
+            return UserPostSerializer
+
+    permission_classes = [IsCreationOrIsAuthenticated]
 
     pagination_class = StandardResultsSetPagination
 
