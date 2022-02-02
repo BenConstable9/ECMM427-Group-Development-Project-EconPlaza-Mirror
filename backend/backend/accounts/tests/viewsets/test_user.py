cdee33a22a76d781798f221daa92331afc57864e
Owen Saunders <owensaundersemail@gmail.com>
2022-02-02 13:50:03 +0000

    Update backend tests

--- backend/backend/accounts/tests/viewsets/test_user.py
+++ backend/backend/accounts/tests/viewsets/test_user.py
@@ -143,23 +143,13 @@ class UserViewsetTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_post_user(self):
-        """Test we get a HTTP 405 response when attempting to add data."""
-        self.client.force_authenticate(self.user_1)
+        """Test we get a HTTP 201 response when attempting to register a user"""
 
         # Set the payload
-        payload = {"first_name": "tester", "username": "test"}
+        payload = {"first_name": "tester", "username": "test", "password": "Te5t@146"}
         response = self.client.post("/v1/users/", payload)
 
-        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
-    def test_post_vouch_with_unauth(self):
-        """Test we get a HTTP 401 response when attempting to add data as we are not authorised."""
-
-        # Set the payload
-        payload = {"first_name": "tester", "username": "test"}
-        response = self.client.post("/v1/users/", payload)
-
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_get_invalid_user(self):
         """Test we get a HTTP 404 response when looking at the detailed view."""
