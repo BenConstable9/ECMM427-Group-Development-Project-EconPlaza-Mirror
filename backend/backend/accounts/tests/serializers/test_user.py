d67c595dbd8cd686a2597c9dae2bd99a55d4c4e4
Owen Saunders <owensaundersemail@gmail.com>
2022-02-02 13:50:03 +0000

    Update backend tests

--- backend/backend/accounts/tests/serializers/test_user.py
+++ backend/backend/accounts/tests/serializers/test_user.py
@@ -1,9 +1,10 @@
 from rest_framework.test import APIRequestFactory, APITestCase
 from rest_framework.request import Request
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 
 from ...serializers import UserSerializer
-
+from ...serializers.user import UserPostSerializer
 
 class UserSerializerTest(APITestCase):
     def setUp(self):
@@ -68,3 +69,62 @@ class UserSerializerTest(APITestCase):
         self.assertEqual(data["username"], self.user_1.username)
         self.assertEqual(data["email"], self.user_1.email)
         self.assertEqual(data["is_staff"], self.user_1.is_staff)
+
+class UserPostSerializerTest(APITestCase):
+    def setUp(self):
+        """Initalise a user."""
+
+        User = get_user_model()
+
+        self.user_1 = User.objects.create(
+            username="user_1",
+            first_name="Test",
+            last_name="User",
+            email="admin@test.com",
+            password="Orange@132",
+        )
+
+        # Add the request context
+        factory = APIRequestFactory()
+        request = factory.get("/accounts/users")
+
+        serializer_context = {
+            "request": Request(request),
+        }
+
+        # Serialise the data
+        self.serializer = UserPostSerializer(
+            instance=self.user_1, context=serializer_context
+        )
+
+    def test_contains_expected_fields(self):
+        """Test the serialiser returns the expected fields."""
+
+        data = self.serializer.data
+
+        self.assertEqual(
+            set(data.keys()),
+            set(
+                [
+                    "id",
+                    "username",
+                    "email",
+                    "first_name",
+                    "last_name",
+                ]
+            ),
+        )
+
+        # Check we aren't exposing fields we don't want to
+        self.assertNotIn("password", data.keys())
+
+    def test_equal_data(self):
+        """Test the serialiser returns the expected values."""
+
+        data = self.serializer.data
+
+        self.assertEqual(data["id"], self.user_1.id)
+        self.assertEqual(data["first_name"], self.user_1.first_name)
+        self.assertEqual(data["last_name"], self.user_1.last_name)
+        self.assertEqual(data["username"], self.user_1.username)
+        self.assertEqual(data["email"], self.user_1.email)
