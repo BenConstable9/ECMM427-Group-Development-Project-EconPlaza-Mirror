5dbb5ea4ab68ffb1516a10b978c1d816d2241392
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-02-07 16:30:22 +0000

    Refactor plaza box header if-else

--- frontend/frontend/components/plaza/PlazaBoxHeader.vue
+++ frontend/frontend/components/plaza/PlazaBoxHeader.vue
@@ -13,22 +13,22 @@
                 {{ name }}
             </NuxtLink>
         </h3>
-        <member
-            v-if="isMember"
-            class="flex-initial md:flex-none lg:flex-initial"
-        />
-        <button v-else class="rounded-full bg-gray-50 p-3" @click="plazaJoin()">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5 text-primary mx-auto"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-            >
-                <path
-                    d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"
-                />
-            </svg>
-        </button>
+        <template v-if="isMember">
+            <member class="flex-initial md:flex-none lg:flex-initial" />
+        </template>
+        <template v-else>
+            <button class="rounded-full bg-gray-50 p-3" @click="plazaJoin()">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5 text-primary mx-auto"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                >
+                    <path
+                        d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"
+                    />
+                </svg></button
+        ></template>
     </div>
 </template>
 
