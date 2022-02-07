11174348f856d1e18b9da16f3d336bd3eb9c096b
Owen Saunders <owensaundersemail@gmail.com>
2022-02-07 17:32:18 +0000

    Change recaptcha decorator to inline (serializer)

--- backend/backend/accounts/serializers/user.py
+++ backend/backend/accounts/serializers/user.py
@@ -6,8 +6,8 @@ from django.contrib.auth.password_validation import validate_password
 
 from django.core.exceptions import ValidationError
 from rest_framework.exceptions import ValidationError as DRFValidationError
+from utils.recaptcha import is_recaptcha_valid
 
-from utils.recaptcha import human_required
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -35,24 +35,25 @@ class UserPostSerializer(serializers.HyperlinkedModelSerializer):
             "email",
             "first_name",
             "last_name",
-            "institution",
+            "institutional_affiliation",
             "password",
         ]
         extra_kwargs = {
             "password": {"write_only": True, "required": False},
         }
 
-    @human_required
     def create(self, validated_data):
-        try:
-            password = validated_data.get("password")
-            validate_password(validated_data["password"])
-            validated_data["password"] = make_password(password)
-        except ValidationError as e:
-            raise DRFValidationError(e.messages)
-        except KeyError as e:
-            return Response(
-                {"detail": "Password field is required."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        return super().create(validated_data)
+        print(self.context)
+        request = self.context.get('request', None)
+        if is_recaptcha_valid(request):
+            try:
+                password = validated_data.get("password")
+                validate_password(validated_data["password"])
+                validated_data["password"] = make_password(password)
+            except ValidationError as e:
+                raise DRFValidationError(e.messages)
+            except KeyError as e:
+                raise DRFValidationError(detail="Bad Password.")
+            return super().create(validated_data)
+        else:
+            raise DRFValidationError(detail="Recaptcha Failed.")
