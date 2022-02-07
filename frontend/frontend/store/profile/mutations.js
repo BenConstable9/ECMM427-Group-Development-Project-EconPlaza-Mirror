baa54e075de7d0ff5bc593bc625df3189e8f329a
Owen Saunders <owensaundersemail@gmail.com>
2022-02-07 13:25:41 +0000

    Remove profile folder

--- frontend/frontend/store/profile/mutations.js
+++
@@ -1,8 +0,0 @@
-export default {
-    setProfileList(state, profiles) {
-        state.all = profiles
-    },
-    setCurrentProfile(state, profile) {
-        state.currentProfile = profile
-    },
-}
