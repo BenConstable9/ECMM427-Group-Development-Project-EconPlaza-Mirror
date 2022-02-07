95c6c8d4331b4448fe2cf79d6d9bba817d87de60
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-02-07 16:51:58 +0000

    Tweak plaza box description text

--- frontend/frontend/components/plaza/PlazaBoxDetails.vue
+++ frontend/frontend/components/plaza/PlazaBoxDetails.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="h-20 truncate bg-gray-50 px-5 py-3">
+    <div class="text-sm text-gray-700 truncate bg-gray-50 px-5 py-4">
         {{ description }}
     </div>
 </template>
