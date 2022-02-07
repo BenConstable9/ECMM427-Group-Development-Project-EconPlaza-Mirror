0223a9896d51915871008b71081e480d928c4b3e
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-02-07 17:28:37 +0000

    Use smaller circles for plaza box icons

--- frontend/frontend/components/plaza/PlazaBoxHeader.vue
+++ frontend/frontend/components/plaza/PlazaBoxHeader.vue
@@ -15,7 +15,7 @@
             </NuxtLink>
         </h3>
         <template v-if="isMember">
-            <div class="rounded-full bg-gray-100 p-3">
+            <div class="rounded-full bg-gray-100 p-2">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     class="h-5 w-5 text-primary mx-auto"
@@ -38,7 +38,7 @@
                     hover:text-secondary
                     bg-gray-100
                     duration-100
-                    p-3
+                    p-2
                 "
                 @click="plazaJoin()"
             >
