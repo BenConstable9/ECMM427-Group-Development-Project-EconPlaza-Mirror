9892a20589f9e4b207c1929d0ea7de6cf65dea50
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-02-07 16:47:23 +0000

    Change icons in plaza box header

--- frontend/frontend/components/plaza/PlazaBoxHeader.vue
+++ frontend/frontend/components/plaza/PlazaBoxHeader.vue
@@ -17,7 +17,20 @@
             </NuxtLink>
         </h3>
         <template v-if="isMember">
-            <member class="flex-initial md:flex-none lg:flex-initial" />
+            <div class="rounded-full bg-gray-100 p-3">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5 text-primary mx-auto"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                        clip-rule="evenodd"
+                    />
+                </svg>
+            </div>
         </template>
         <template v-else>
             <button
@@ -37,7 +50,9 @@
                     fill="currentColor"
                 >
                     <path
-                        d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"
+                        fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z"
+                        clip-rule="evenodd"
                     />
                 </svg>
             </button>
@@ -47,13 +62,9 @@
 
 <script>
 import { mapActions } from 'vuex'
-import Member from '~/components/labels/Member'
 
 export default {
     name: 'PlazaBoxHeader',
-    components: {
-        Member,
-    },
     props: {
         slug: { type: String, default: '' },
         name: { type: String, default: '' },
