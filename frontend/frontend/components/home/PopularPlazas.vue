756773bfb22e79def249767ab79b3102cee2bca4
Owen Saunders <owensaundersemail@gmail.com>
2022-02-08 17:14:15 +0000

    Add alternate background colour

--- frontend/frontend/components/home/PopularPlazas.vue
+++ frontend/frontend/components/home/PopularPlazas.vue
@@ -4,10 +4,11 @@
             <popular-plazas-header />
             <ol class="list-inside">
                 <popular-plazas-row
-                    v-for="plaza in plazas"
+                    v-for="(plaza, index) in plazas"
                     :key="plaza.plaza__name"
-                    :title="plaza.plaza__name"
+                    :name="plaza.plaza__name"
                     :slug="plaza.plaza__slug"
+                    :class="{ 'bg-gray-50': index % 2 }"
                 />
             </ol>
         </ul>
@@ -29,7 +30,7 @@ export default {
     methods: {
         async getPlazas() {
             const response = await this.$axios.get(PLAZAS.POPULAR())
-            this.plazas = response.data
+            this.plazas = response.data.slice(0, 5)
         },
     },
 }
