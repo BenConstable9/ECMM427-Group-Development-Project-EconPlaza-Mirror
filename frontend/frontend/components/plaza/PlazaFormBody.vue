e6f9f3c9acc600c9f4bf1b1424969d863ec52987
Ben Constable <ben-constable@outlook.com>
2022-02-15 17:15:07 +0000

    Add plaza form body

--- frontend/frontend/components/plaza/PlazaFormBody.vue
+++ frontend/frontend/components/plaza/PlazaFormBody.vue
@@ -1,54 +1,16 @@
 <template>
     <div>
-        <ul
-            class="
-                w-full
-                border
-                rounded-r-2xl rounded-b-2xl
-                overflow-hidden
-                shadow-md
-            "
-        >
+        <ul class="w-full overflow-hidden">
             <Error v-if="error" :message="error" />
             <div
-                id="post-heading"
+                id="plaza-heading"
                 class="flex space-x-3 items-center px-5 py-3"
             >
-                <form class="space-y-4 w-full" @submit.prevent="postNew">
-                    <div>
-                        Post as:
-                        <select
-                            :value="profile.id"
-                            :disabled="post.isDisabled"
-                            class="
-                                w-full
-                                p-4
-                                text-m
-                                bg-white
-                                focus:outline-none
-                                border border-gray-200
-                                rounded-r-2xl rounded-b-2xl
-                                text-blue-600
-                                shadow
-                                disabled:bg-gray-100
-                                disabled:text-gray-500
-                                disabled:border-gray-400
-                            "
-                            @change="profileSwitch($event)"
-                        >
-                            <option
-                                v-for="item in profiles"
-                                :key="item.id"
-                                :value="item.id"
-                            >
-                                {{ item.display_name }}
-                            </option>
-                        </select>
-                    </div>
+                <form class="space-y-4 w-full" @submit.prevent="plazaNew">
                     <div>
                         <input
-                            v-model="post.title"
-                            :disabled="post.isDisabled"
+                            v-model="plaza.name"
+                            :disabled="plaza.isDisabled"
                             class="
                                 w-full
                                 p-4
@@ -56,47 +18,82 @@
                                 bg-white
                                 focus:outline-none
                                 border border-gray-200
-                                rounded-r-2xl rounded-b-2xl
-                                text-blue-600
-                                shadow
+                                rounded-lg
+                                text-primary
                                 disabled:bg-gray-100
                                 disabled:text-gray-500
                                 disabled:border-gray-400
                             "
                             type="text"
-                            placeholder="Post Title"
-                            maxlength="140"
+                            placeholder="Plaza Name"
+                            maxlength="32"
                         />
                     </div>
                     <div>
-                        <Editor
-                            v-model="post.content"
-                            :disabled="post.isDisabled"
-                            placeholder="Type your post here..."
+                        <input
+                            v-model="plaza.slug"
+                            :disabled="plaza.isDisabled"
+                            class="
+                                w-full
+                                p-4
+                                text-m
+                                bg-white
+                                focus:outline-none
+                                border border-gray-200
+                                rounded-lg
+                                text-primary
+                                disabled:bg-gray-100
+                                disabled:text-gray-500
+                                disabled:border-gray-400
+                            "
+                            type="text"
+                            placeholder="Plaza Slug"
+                            maxlength="32"
                         />
+                    </div>
+                    <div>
+                        <textarea
+                            v-model="plaza.description"
+                            :disabled="plaza.isDisabled"
+                            class="
+                                w-full
+                                p-4
+                                text-m
+                                bg-white
+                                focus:outline-none
+                                border border-gray-200
+                                rounded-lg
+                                text-primary
+                                disabled:bg-gray-100
+                                disabled:text-gray-500
+                                disabled:border-gray-400
+                            "
+                            type="text"
+                            placeholder="Plaza Description"
+                            maxlength="2800"
+                        >
+                        </textarea>
                     </div>
                     <div
                         v-if="
-                            post.title.length > 0 &&
-                            post.title.length <= 140 &&
-                            post.content.length > 0 &&
-                            post.content.length <= 2800
+                            plaza.name.length > 0 &&
+                            plaza.name.length <= 32 &&
+                            plaza.description.length > 0 &&
+                            plaza.description.length <= 2800
                         "
                     >
                         <button
                             type="submit"
-                            :disabled="post.isDisabled"
+                            :disabled="plaza.isDisabled"
                             class="
                                 w-full
                                 py-4
-                                bg-blue-600
-                                hover:bg-blue-800
+                                bg-primary
+                                hover:bg-secondary
                                 rounded-lg
                                 text-m
                                 font-bold
-                                border border-blue-600
                                 text-white
-                                shadow
                                 transition
                                 duration-200
                                 disabled:bg-gray-100
@@ -104,7 +101,7 @@
                                 disabled:border-gray-400
                             "
                         >
-                            Submit Post
+                            Submit plaza
                         </button>
                     </div>
                 </form>
@@ -114,7 +111,6 @@
 </template>
 
 <script>
-import { mapGetters, mapActions, mapMutations } from 'vuex'
 import { PLAZAS } from '../../api-routes'
 import Error from '~/components/messages/Error'
 
@@ -124,59 +120,30 @@ export default {
     },
     data() {
         return {
-            post: {
-                title: '',
-                content: '',
+            plaza: {
+                name: '',
+                slug: '',
+                description: '',
                 permissions: '{}',
-                reactions: '{}',
                 isDisabled: false,
             },
             error: null,
         }
     },
-    computed: {
-        ...mapGetters({
-            profile: 'profiles/currentProfile',
-            profiles: 'profiles/allProfiles',
-            authenticatedUser: 'authenticatedUser',
-        }),
-    },
-    async created() {
-        await this.getAllProfiles()
-    },
     methods: {
-        ...mapActions({
-            getCurrentProfile: 'profiles/getCurrentProfile',
-            getAllProfiles: 'profiles/getAllProfiles',
-        }),
-        ...mapMutations({
-            setCurrentProfile: 'profiles/setCurrentProfile',
-        }),
-        profileSwitch(e) {
-            // Get the profile that matches
-            const newProfile = this.profiles.find(
-                ({ id }) => id === Number(e.target.value)
-            )
-            // Update the store
-            this.setCurrentProfile(newProfile)
-        },
-        async postNew() {
-            this.post.isDisabled = true
+        async plazaNew() {
+            this.plaza.isDisabled = true
 
             // Send to server
             await this.$axios
-                .post(PLAZAS.POSTS(this.$route.params.plazas), {
-                    title: this.post.title,
-                    content: this.post.content,
-                    user: this.$store.getters.authenticatedUser.id,
-                    profile: this.profile.id,
-                    reactions: this.post.reactions,
-                    permissions: this.post.permissions,
+                .plaza(PLAZAS.ALL(), {
+                    name: this.plaza.name,
+                    description: this.plaza.description,
+                    slug: this.plaza.slug,
+                    permissions: this.plaza.permissions,
                 })
                 .then((response) => {
-                    this.$router.push(
-                        `/plazas/${this.$route.params.plazas}/posts/${response.data.id}/`
-                    )
+                    this.$router.push(`/plazas/${response.data.slug}/`)
                 })
                 .catch((response) => {
                     if (
@@ -190,7 +157,7 @@ export default {
                         this.error = 'Unable to process request.'
                     }
 
-                    this.post.isDisabled = false
+                    this.plaza.isDisabled = false
                 })
         },
     },
