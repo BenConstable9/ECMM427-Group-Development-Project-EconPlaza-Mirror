94b9ba1425d458bd6b1d322eab90a1d22359dd1f
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-02-16 15:51:39 +0000

    Update error message styling

--- frontend/frontend/components/messages/Error.vue
+++ frontend/frontend/components/messages/Error.vue
@@ -1,32 +1,35 @@
 <template>
     <div
+        role="alert"
         class="
-            w-full
-            px-5
+            bg-red-100
+            border-t-4 border-red-500
+            text-red-700
+            duration-100
+            px-3
             py-3
-            bg-red-600
-            text-m
-            font-bold
-            text-white
-            flex
-            items-center
         "
     >
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 inline mr-1"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-        >
-            <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-        </svg>
-        Error: {{ message }}
+        <div class="flex space-x-3 text-red-500">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+            </svg>
+            <div id="message">
+                <h3 class="text-red-900 font-semibold">Error</h3>
+                <p class="text-sm text-red-900">{{ message }}</p>
+            </div>
+        </div>
     </div>
 </template>
 
