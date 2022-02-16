7a99662c6e2c961b2861d75f6a0f9e3201d2c7ea
Owen Saunders <owensaundersemail@gmail.com>
2022-02-16 02:27:07 +0000

    Remove Docstring Typo

--- backend/backend/utils/permissions.py
+++ backend/backend/utils/permissions.py
@@ -72,7 +72,7 @@ class ContainsPlazaURLVerifiedMember(AllowAny):
 
 class ContainsPlazaURLVerified(AllowAny):
     """
-    Custom permission that checks we are accessing /plazas/ and we are verified na.
+    Custom permission that checks we are accessing /plazas/ and we are verified.
     """
 
     def has_permission(self, request, view):
