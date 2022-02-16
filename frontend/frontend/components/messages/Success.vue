e53ad1a02ac908d009d9a275ef2674e84273516c
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-02-16 16:00:32 +0000

    Update success message styling

--- frontend/frontend/components/messages/Success.vue
+++ frontend/frontend/components/messages/Success.vue
@@ -1,20 +1,35 @@
 <template>
-    <div class="w-full px-5 py-3 bg-green-600 text-m font-bold text-white">
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 inline mr-1"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-        >
-            <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-        </svg>
-        Success: {{ message }}
+    <div
+        role="alert"
+        class="
+            bg-green-100
+            border-t-4 border-green-500
+            text-green-700
+            duration-100
+            px-3
+            py-3
+        "
+    >
+        <div class="flex space-x-3 text-green-500">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+            </svg>
+            <div id="message">
+                <h3 class="text-green-900 font-semibold">Success</h3>
+                <p class="text-sm text-green-900">{{ message }}</p>
+            </div>
+        </div>
     </div>
 </template>
 
