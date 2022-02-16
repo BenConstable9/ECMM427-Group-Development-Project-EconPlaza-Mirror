e7f2d29544ca47f80028c1211aa14c9a44738693
Ben Constable <ben-constable@outlook.com>
2022-02-16 15:29:27 +0000

    Neaten the comment success box

--- frontend/frontend/components/messages/Success.vue
+++ frontend/frontend/components/messages/Success.vue
@@ -1,16 +1,19 @@
 <template>
-    <div
-        class="
-            w-full
-            p-4
-            bg-green-700
-            rounded-r-2xl rounded-b-2xl
-            text-m
-            font-bold
-            text-white
-            shadow
-        "
-    >
+    <div class="w-full px-5 py-3 bg-green-600 text-m font-bold text-white">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 inline mr-1"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+        >
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+        </svg>
         Success: {{ message }}
     </div>
 </template>
