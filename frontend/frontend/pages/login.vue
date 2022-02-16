e80a3f55e34019ef4ac2a43d666a0308668a5433
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-02-16 15:14:45 +0000

    Update login page styling

--- frontend/frontend/pages/login.vue
+++ frontend/frontend/pages/login.vue
@@ -1,119 +1,90 @@
 <template>
-    <main>
-        <div class="container mx-auto flex justify-center items-center">
-            <div
-                class="
-                    max-w-md
-                    w-full
-                    bg-gray-200
-                    rounded-r-2xl rounded-b-2xl
-                    p-4
-                    m-10
-                    space-y-4
-                    shadow-md
-                "
+    <div class="w-full max-w-md bg-gray-50 rounded-lg border shadow-sm mx-auto">
+        <form
+            class="flex flex-col mx-8 mt-8 border-b"
+            @submit.prevent="userLogin"
+        >
+            <h1 class="text-2xl text-gray-800 font-semibold mx-auto">
+                EconPlaza
+            </h1>
+            <Error v-if="error" :message="error" />
+
+            <label class="text-gray-800 mt-8 mb-3" for="username"
+                >Username</label
             >
-                <div class="mb-4">
-                    <h2 class="text-xl font-bold">Login to EconPlaza</h2>
-                </div>
-                <Error v-if="error" :message="error" />
-                <form class="space-y-4" @submit.prevent="userLogin">
-                    <div>
-                        <input
-                            v-model="login.username"
-                            :disabled="login.isDisabled"
-                            class="
-                                w-full
-                                p-4
-                                text-m
-                                bg-white
-                                focus:outline-none
-                                border border-gray-200
-                                rounded-r-2xl rounded-b-2xl
-                                text-blue-600
-                                shadow
-                                disabled:bg-gray-100
-                                disabled:text-gray-500
-                                disabled:border-gray-400
-                            "
-                            type="text"
-                            placeholder="Username"
-                        />
-                    </div>
-                    <div>
-                        <input
-                            v-model="login.password"
-                            :disabled="login.isDisabled"
-                            class="
-                                w-full
-                                p-4
-                                text-m
-                                bg-white
-                                focus:outline-none
-                                border border-gray-200
-                                rounded-r-2xl rounded-b-2xl
-                                text-blue-600
-                                shadow
-                                disabled:bg-gray-100
-                                disabled:text-gray-500
-                                disabled:border-gray-400
-                            "
-                            type="password"
-                            placeholder="Password"
-                        />
-                    </div>
-                    <div class="field">
-                        <recaptcha />
-                    </div>
-                    <div>
-                        <button
-                            type="submit"
-                            :disabled="login.isDisabled"
-                            class="
-                                w-full
-                                py-4
-                                bg-blue-600
-                                hover:bg-blue-800
-                                rounded-r-2xl rounded-b-2xl
-                                text-m
-                                font-bold
-                                border border-blue-600
-                                text-white
-                                shadow
-                                transition
-                                duration-200
-                                disabled:bg-gray-100
-                                disabled:text-gray-500
-                                disabled:border-gray-400
-                            "
-                        >
-                            Login
-                        </button>
-                    </div>
-                </form>
-                <button
-                    type="submit"
-                    class="
-                        w-full
-                        py-4
-                        bg-blue-600
-                        hover:bg-blue-800
-                        rounded-r-2xl rounded-b-2xl
-                        text-m
-                        font-bold
-                        border border-blue-600
-                        text-white
-                        shadow
-                        transition
-                        duration-200
-                    "
-                    @click="register"
-                >
-                    Register
-                </button>
+
+            <input
+                v-model="login.username"
+                :disabled="login.isDisabled"
+                class="
+                    rounded-lg
+                    border
+                    px-3
+                    py-2
+                    disabled:bg-gray-100 disabled:text-gray-500
+                "
+                type="text"
+                name="username"
+            />
+
+            <label class="text-gray-800 mt-5 mb-3" for="password"
+                >Password</label
+            >
+
+            <input
+                v-model="login.password"
+                :disabled="login.isDisabled"
+                class="
+                    rounded-lg
+                    border
+                    px-3
+                    py-2
+                    disabled:bg-gray-100 disabled:text-gray-500
+                "
+                type="password"
+                name="password"
+            />
+
+            <label class="text-gray-800 mt-5 mb-3">Verification</label>
+
+            <div class="field">
+                <recaptcha />
             </div>
+
+            <button
+                :disabled="login.isDisabled"
+                class="
+                    rounded-lg
+                    border
+                    text-gray-50
+                    bg-primary
+                    hover:bg-blue-500
+                    duration-100
+                    px-3
+                    py-2
+                    mt-10
+                    mb-8
+                    disabled:bg-gray-100
+                    disabled:text-gray-500
+                    disabled:border-gray-400
+                "
+                type="submit"
+            >
+                Log in
+            </button>
+        </form>
+        <div class="flex flex-col mx-5 mt-8 mb-8">
+            <h1 class="text-gray-800 mx-auto">
+                Need an account?
+                <NuxtLink
+                    :to="`/register/`"
+                    class="text-blue-700 hover:underline"
+                >
+                    Sign up
+                </NuxtLink>
+            </h1>
         </div>
-    </main>
+    </div>
 </template>
 
 <script>
