11971d527ce5fb0cc8a90c70a007712adf574ff0
Sam Bebbington <sambebbington23@gmail.com>
2022-02-17 10:57:23 +0000

    Add Coverage Regex

--- backend/backend/run-tests.sh
+++ backend/backend/run-tests.sh
@@ -11,7 +11,7 @@ export TEST_OUTPUT_DIR=/test-results
 export TEST_OUTPUT_FILE_NAME=results.xml
 export DEVELOPMENT=True
 
-
+# Run tests and generate coverage reports
 coverage run --source='.' manage.py test
 coverage report
 coverage xml -o $TEST_OUTPUT_DIR/coverage.xml
