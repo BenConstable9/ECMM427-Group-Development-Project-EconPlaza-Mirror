a8543a1a01a5129699cb2706d5a65cde7ebb34d4
Ben Constable <ben-constable@outlook.com>
2022-02-22 20:28:02 +0000

    Add new orders

--- frontend/frontend/store/plazas/posts/state.js
+++ frontend/frontend/store/plazas/posts/state.js
@@ -12,8 +12,12 @@ export default () => {
             sortOptions: [
                 { key: '-id', name: 'Newest' },
                 { key: 'id', name: 'Oldest' },
+                { key: '-last_activity', name: 'Newest Activity' },
+                { key: 'last_activity', name: 'Oldest Activity' },
                 { key: '-views', name: 'Most Views' },
                 { key: 'views', name: 'Least Views' },
+                { key: '-replies', name: 'Most Replies' },
+                { key: 'replies', name: 'Least Replies' },
             ],
         },
         currentPost: undefined,
