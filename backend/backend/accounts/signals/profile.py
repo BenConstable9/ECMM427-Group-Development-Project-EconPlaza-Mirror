be0966f273bd48d84f38ced603450540635e5ecb
Ben Constable <ben-constable@outlook.com>
2022-02-22 16:19:45 +0000

    Automatically make annoymous profile

--- backend/backend/accounts/signals/profile.py
+++ backend/backend/accounts/signals/profile.py
@@ -7,6 +7,12 @@ from ..models import User, Profile
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
+        # Create a standard profile
         display_name = instance.first_name + " " + instance.last_name
 
         Profile.objects.create(user=instance, display_name=display_name)
+
+        # Create an annoymous profile
+        Profile.objects.create(
+            user=instance, display_name="Annoymous", global_annoymous=True
+        )
