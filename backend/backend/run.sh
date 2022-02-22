7c68906f6db0b06f91406ce0e5d8d6a052f9b283
GTBebbo <bebbosb@googlemail.com>
2022-02-22 20:41:00 +0000

    Add Log File Locations to Gunicorn Run Command

--- backend/backend/run.sh
+++ backend/backend/run.sh
@@ -9,4 +9,4 @@ python manage.py collectstatic --no-input
 
 # Start Server
 
-gunicorn -b 0.0.0.0:80 --workers 2 backend.wsgi
+gunicorn -b 0.0.0.0:80 --workers 2 --access-logfile /var/log/econplaza/gunicorn-access.log --error-logfile /var/log/econplaza/gunicorn-error.log backend.wsgi
