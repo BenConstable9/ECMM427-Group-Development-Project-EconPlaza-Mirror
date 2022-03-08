385b3812302ca0cd38f4a5401de6e5d17066f9dd
Ben Constable <ben-constable@outlook.com>
2022-03-08 10:03:19 +0000

    Update the formatting help part

--- frontend/frontend/components/post/Editor.vue
+++ frontend/frontend/components/post/Editor.vue
@@ -12,46 +12,21 @@
         <div class="flex flex-row border-b-2 bg-gray-50 items-center">
             <div class="py-3 px-4 m-2 mr-0 flex-grow text-l">{{ title }}</div>
             <div
-                class="cursor-pointer py-3 px-4 m-2 mr-0 rounded transition"
-                :class="{
-                    'text-gray-50': !showPreview,
-                    'text-gray-600': showPreview,
-                    'bg-primary': !showPreview,
-                    'bg-gray-200': showPreview,
-                }"
-                @click="showPreview = false"
-            >
-                Editor
-            </div>
-            <div
-                class="cursor-pointer py-3 px-4 m-2 rounded transition"
-                :class="{
-                    'text-gray-50': showPreview,
-                    'text-gray-600': !showPreview,
-                    'bg-primary': showPreview,
-                    'bg-gray-200': !showPreview,
-                }"
-                @click="showPreview = true"
-            >
-                Preview
-            </div>
-            <div
                 class="
                     cursor-pointer
                     py-3
-                    px-3
-                    mr-2
                     rounded
                     transition
                     text-gray-400
+                    flex
+                    inline-center
                     hover:text-gray-600
                 "
                 @click="showMarkdownHelp = true"
             >
-                Markdown Help
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    class="h-6 w-6 inline"
+                    class="h-6 w-6 inline mr-1"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -63,6 +38,31 @@
                         d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                     />
                 </svg>
+                Formatting Help
+            </div>
+            <div
+                class="cursor-pointer py-3 px-4 mx-2 rounded transition"
+                :class="{
+                    'text-gray-50': !showPreview,
+                    'text-gray-600': showPreview,
+                    'bg-primary': !showPreview,
+                    'bg-gray-200': showPreview,
+                }"
+                @click="showPreview = false"
+            >
+                Editor
+            </div>
+            <div
+                class="cursor-pointer py-3 px-4 mr-4 rounded transition"
+                :class="{
+                    'text-gray-50': showPreview,
+                    'text-gray-600': !showPreview,
+                    'bg-primary': showPreview,
+                    'bg-gray-200': !showPreview,
+                }"
+                @click="showPreview = true"
+            >
+                Preview
             </div>
         </div>
         <textarea
