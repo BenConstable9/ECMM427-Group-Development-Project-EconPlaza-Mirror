d2dc71d067725d71db464dfb81acfd5e7bacc975
Ben Constable <ben-constable@outlook.com>
2022-03-08 09:54:34 +0000

    Add to default files

--- frontend/frontend/store/plazas/posts/comments/default.js
+++ frontend/frontend/store/plazas/posts/comments/default.js
@@ -3,10 +3,16 @@ export const defaultState = () => {
         comments: undefined,
         pagination: {
             page: undefined,
+            desiredSort: 'id',
             desiredSize: 10,
             returnedSize: undefined,
+            returnedSort: undefined,
             next: undefined,
             previous: undefined,
+            sortOptions: [
+                { key: '-id', name: 'Newest' },
+                { key: 'id', name: 'Oldest' },
+            ],
         },
         currentPost: undefined,
         currentPlaza: undefined,
