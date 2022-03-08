bd0aa7ad4f2ad035ec8a3593f7a9a5db05d6910a
Ben Constable <ben-constable@outlook.com>
2022-03-08 10:32:03 +0000

    Neaten up comment box and add reply button

--- frontend/frontend/components/post/CommentTableRow.vue
+++ frontend/frontend/components/post/CommentTableRow.vue
@@ -2,23 +2,54 @@
     <div id="comment-info" class="flex items-center px-5 py-4">
         <div class="flex flex-1 flex-col space-y-1">
             <MarkdownViewer :content="content" />
-            <p class="flex space-x-1 mt-1 mx-5 border-t py-3">
-                <span class="text-xs text-gray-700"
-                    >By
-                    <NuxtLink
-                        v-if="includeAuthorLink"
-                        :to="authorLink"
-                        class="font-semibold hover:underline"
+            <div class="flex flex-row border-t py-3 mx-5">
+                <p class="flex-1 flex space-x-1 mt-1">
+                    <span class="text-xs text-gray-700"
+                        >By
+                        <NuxtLink
+                            v-if="includeAuthorLink"
+                            :to="authorLink"
+                            class="font-semibold hover:underline"
+                        >
+                            {{ author }}
+                        </NuxtLink>
+                        <span v-else class="font-semibold">{{ author }}</span>
+                    </span>
+                    <span class="text-xs text-gray-700 font-semibold"
+                        >&middot;</span
                     >
-                        {{ author }}
-                    </NuxtLink>
-                    <span v-else class="font-semibold">{{ author }}</span>
-                </span>
-                <span class="text-xs text-gray-700 font-semibold"
-                    >&middot;</span
-                >
-                <span class="text-xs text-gray-700">{{ time }}</span>
-            </p>
+                    <span class="text-xs text-gray-700">{{ time }}</span>
+                </p>
+                <div class="flex-initial flex space-x-1 mt-1">
+                    <div
+                        class="
+                            cursor-pointer
+                            rounded
+                            transition
+                            text-gray-400
+                            flex
+                            inline-center
+                            text-xs
+                            hover:text-gray-600
+                        "
+                        @click="showMarkdownHelp = true"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-4 w-4 inline mr-1"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 017 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                                clip-rule="evenodd"
+                            />
+                        </svg>
+                        Reply
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
