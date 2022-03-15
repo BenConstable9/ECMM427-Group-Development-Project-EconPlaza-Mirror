fb910a84faed4de6922148a5d1f338b6cab82e7e
Ben Constable <ben-constable@outlook.com>
2022-03-15 17:46:12 +0000

    Add a recursive relation on the child

--- backend/backend/plazas/serializers/comment.py
+++ backend/backend/plazas/serializers/comment.py
@@ -5,10 +5,14 @@ import json
 from accounts.serializers import ProfileSerializer
 from ..models import Comment
 
+class RecursiveField(serializers.Serializer):
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
 
 class CommentSerializer(serializers.ModelSerializer):
     reactions = serializers.JSONField()
-    children = serializers.SerializerMethodField("get_comment_children")
+    children = RecursiveField(many=True, read_only=True)
 
     class Meta:
         model = Comment
