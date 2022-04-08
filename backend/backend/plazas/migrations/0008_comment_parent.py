092baa7f41f372958dbd22211759f235a16b3801
Ben Constable <ben-constable@outlook.com>
2022-04-08 10:20:34 +0100

    Update 0008_comment_parent.py

--- backend/backend/plazas/migrations/0008_comment_parent.py
+++ backend/backend/plazas/migrations/0008_comment_parent.py
@@ -7,13 +7,20 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('plazas', '0007_alter_plaza_slug'),
+        ("plazas", "0007_alter_plaza_slug"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='comment',
-            name='parent',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='children', to='plazas.comment', verbose_name="Comment's Parent Comment"),
+            model_name="comment",
+            name="parent",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="children",
+                to="plazas.comment",
+                verbose_name="Comment's Parent Comment",
+            ),
         ),
     ]
